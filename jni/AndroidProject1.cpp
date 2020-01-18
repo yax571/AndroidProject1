0ad2645a9c107af7fe27dd70e45585335c64ab10
yax571 <yax571@163.com>
2020-01-18 17:18:18 +0800

    Add project files.

---
+++ jni/AndroidProject1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	std::string aa = "";
+	printf("Hello, World!\n");	//Set a breakpoint here or press F10 to step into.
+	return 0;
+}
