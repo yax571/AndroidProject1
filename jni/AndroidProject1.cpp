ace7dc72cc7cbb31d3d0e3b204ae67b89a68d67a
yax571 <yax571@163.com>
2020-01-18 21:09:00 +0800

    已验证arm64 真机 和 avd x86 均可exe 单步调试 和下断点 以及反汇编

--- jni/AndroidProject1.cpp
+++ jni/AndroidProject1.cpp
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <string>
+#include <android/log.h>
 
+#include <unistd.h>
+
+#include <signal.h>
+#include "debugbreak.h"
+#include <string.h>
 int main(int argc, char **argv)
 {
-	std::string aa = "";
-	printf("Hello, World!\n");	//Set a breakpoint here or press F10 to step into.
+	//debug_break();
+	raise(SIGTRAP);
+	sleep(3);
+	//__android_log_assert("", "int3", "");
+	//__builtin_trap();
+	std::string aa = "Hello, World!\n";
+	char *sub = strstr((char*)aa.c_str(), "World");
+	char tmp[_POSIX_PATH_MAX] = { 0 };
+	strcpy(tmp, sub);
+	printf("%s",sub);	//Set a breakpoint here or press F10 to step into.
+	
 	return 0;
 }
